108eb04b680c30a34a77bdcc80df887e036229db
Gseven <gse7en2010@163.com>
2014-07-10 00:05:38 +0800

    1.帮方少格式化了com.js 2.刷存在感

--- js/com.js
+++ js/com.js
@@ -1,59 +1,63 @@
-var com={
-	signEdit:function(){
-		$(".c-h-s-btn").click(function(){
-			$(".c-h-s-btn,.c-h-s-text").hide();
-			$(".c-h-sign-edit").show();
-		});
-		$(".c-h-sign-edit").find("a,button").click(function(){
-			$(".c-h-s-btn,.c-h-s-text").show();
-			$(".c-h-sign-edit").hide();
-		});
-	},
-    tab:function(btnObj,conObj,params){
-      /**可公用组件：tab切换
-        * @constructor tab
-        * @param {jquery Object||String} btnObj 按钮对象
-        * @param {jquery Object||String||Array} conObj 内容对象,如多个可为Array（如：["#more li","#con li"]）
-    **/
-      function chk(val,defaultVal){
-          return (typeof params!="undefined" &&typeof params[val]!="undefined")?params[val]:defaultVal;
-      }
-        function activeBtn(btnObj){
+var com = {
+    signEdit: function () {
+        $(".c-h-s-btn").click(function () {
+            $(".c-h-s-btn,.c-h-s-text").hide();
+            $(".c-h-sign-edit").show();
+        });
+        $(".c-h-sign-edit").find("a,button").click(function () {
+            $(".c-h-s-btn,.c-h-s-text").show();
+            $(".c-h-sign-edit").hide();
+        });
+    },
+    tab: function (btnObj, conObj, params) {
+        /**可公用组件：tab切换
+         * @constructor tab
+         * @param {jquery Object||String} btnObj 按钮对象
+         * @param {jquery Object||String||Array} conObj 内容对象,如多个可为Array（如：["#more li","#con li"]）
+         **/
+        function chk(val, defaultVal) {
+            return (typeof params != "undefined" && typeof params[val] != "undefined") ? params[val] : defaultVal;
+        }
+
+        function activeBtn(btnObj) {
             $(btnCurrent).removeClass(activeClass);
             btnCurrent = btnObj;
             $(btnObj).addClass(activeClass);
         }
-        function activeFun(num){
-            if($.isFunction(fn)) fn(num);
+
+        function activeFun(num) {
+            if ($.isFunction(fn)) fn(num);
         }
-        function activeCons(num){
+
+        function activeCons(num) {
             if (typeof conObj != "string") {
-                $.each(cons, function(i){
+                $.each(cons, function (i) {
                     $(conCurrent[i]).removeClass(activeClass);
                     conCurrent[i] = this[num];
                     $(this[num]).addClass(activeClass);
                 });
             }
             else {
-                if(conCurrent) $(conCurrent).removeClass(activeClass);
+                if (conCurrent) $(conCurrent).removeClass(activeClass);
                 conCurrent = cons[num];
                 $(cons[num]).addClass(activeClass);
             }
         }
+
         var btns = $(btnObj),
             btnCurrent,
             conCurrent,
             cons,
-            fn = chk("fn",null),
+            fn = chk("fn", null),
             status,
             timer,
-            time = chk("time","180"),
-            index = chk("index",0),
+            time = chk("time", "180"),
+            index = chk("index", 0),
             currentIndex,
-            activeClass = chk("activeClass","current");
+            activeClass = chk("activeClass", "current");
         if (typeof conObj != "string") {
-            cons = [],conCurrent=[];
-            $.each(conObj, function(){
+            cons = [], conCurrent = [];
+            $.each(conObj, function () {
                 cons.push($(this.toString()));
                 conCurrent.push({});
             });
@@ -61,37 +65,38 @@ var com={
         }
         else {
             cons = $(conObj);
-            conCurrent={};
+            conCurrent = {};
         }
-        $.each(btns,function(i){
-            $(this).bind("mouseenter",{"i":i,"o":btns[i]}, function(e){
-                overStatus=true;
-                (function(){
+        $.each(btns, function (i) {
+            $(this).bind("mouseenter", {"i": i, "o": btns[i]},function (e) {
+                overStatus = true;
+                (function () {
                     var btn = e.data.o,
                         index = e.data.i;
-                    timer = setTimeout(function(o){
-                        if (overStatus&&currentIndex!=index) {
-                            currentIndex=index;
+                    timer = setTimeout(function (o) {
+                        if (overStatus && currentIndex != index) {
+                            currentIndex = index;
                             activeBtn(btn);
                             activeCons(index);
                             activeFun(index);
-                        }}, time);
+                        }
+                    }, time);
                 })();
-            }).bind("mouseleave",{"i":i},function(e){
-                clearTimeout(timer);
-                overStatus=false;
-            });
+            }).bind("mouseleave", {"i": i}, function (e) {
+                    clearTimeout(timer);
+                    overStatus = false;
+                });
         });
         activeBtn(btns[index]);
         activeCons(index);
         activeFun(index);
     },
-	init:function(){
-		this.signEdit();
-	}
+    init: function () {
+        this.signEdit();
+    }
 }
-$(function(){
-	com.init();
+$(function () {
+    com.init();
     //上网记录
-    com.tab('.c-tab-btn li','.c-tab-cont');
+    com.tab('.c-tab-btn li', '.c-tab-cont');
 });
