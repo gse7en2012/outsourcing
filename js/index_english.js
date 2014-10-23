1e555eb73cc13bc9b165b23845f1f8f38df275f3
Tim Wen <timwenchina@163.com>
2014-10-23 23:32:55 +0800

    新增英语园地-英语词典查询结果页面
    修复了一些bug，icon的雪碧图后续再合并了

--- js/index_english.js
+++ js/index_english.js
@@ -42,13 +42,39 @@ $(function(){
         });
     });
 
-    // 右侧功公共部分标题点击开关
-    $('.c-m-r-part-toggle .block').toggle(function(){
-        $(this).next().stop().slideUp();
-        $(this).find('em').removeClass('c-m-r-p-icon-minus').addClass('c-m-r-p-icon-plus');
-    },function(){
-        $(this).next().stop().slideDown();
-        $(this).find('em').removeClass('c-m-r-p-icon-plus').addClass('c-m-r-p-icon-minus')
+//    // 右侧功公共部分标题点击开关
+//    $('.c-m-r-part-toggle .block').toggle(function(){
+//        $(this).next().stop().slideUp();
+//        $(this).find('em').removeClass('c-m-r-p-icon-minus').addClass('c-m-r-p-icon-plus');
+//    },function(){
+//        $(this).next().stop().slideDown();
+//        $(this).find('em').removeClass('c-m-r-p-icon-plus').addClass('c-m-r-p-icon-minus')
+//    });
+//
+//    /*
+//    $('.toggle').toggle(function(){
+//        $(this).next().stop().slideUp();
+//        $(this).find('em').removeClass('c-m-r-p-icon-minus').addClass('c-m-r-p-icon-plus');
+//    },function(){
+//        $(this).next().stop().slideDown();
+//        $(this).find('em').removeClass('c-m-r-p-icon-plus').addClass('c-m-r-p-icon-minus')
+//    });
+//    */
+
+    // 公共右侧附加短语中文宽度设置
+    $('.c-m-r2-part3-appendList li').each(function(i) {
+        $(this).find(".c-m-r2-part3-appendList-chinese").css({width:$(this).width()-$(this).find(".c-m-r2-part3-appendList-english").outerWidth(true)-5});
+    });
+
+    // 显示/关闭toggle
+    $('.toggle').click(function() {
+        if(!$(this).hasClass('hide')) {
+            $(this).next().stop().slideUp('fast');
+            $(this).addClass('hide');
+        } else {
+            $(this).next().stop().css({'opacity':'1'}).slideDown('fast');
+            $(this).removeClass('hide');
+        }
     });
 
 });
