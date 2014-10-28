da5de4875ebdc09178918d2e008581888ef65b41
Tim Wen <timwenchina@163.com>
2014-10-28 23:41:49 +0800

    新增投票-热门标签页面，配套内页正在制作中

---
+++ js/index_vote.js
@@ -0,0 +1,30 @@
+$(function() {
+    // 广播广场-大家在说、同城广播、热门转播tab切换
+    com.tab('.c-m-left-nav a', '.c-m-left-main');
+
+    // 广播广场-排行榜页面tab切换
+    $('.c-m-content-box-content-box').each(function(i) {
+        $(this).addClass('tabClass'+i);
+        com.tab('.tabClass' + i + ' .c-m-content-box-content-box-title-navList-a', '.tabClass' + i + ' .c-m-content-box-content-box-title-rankList');
+    });
+
+    // 广播广场-热门标签tab切换
+    com.tab('.c-m-left-navWrap-navList a', '.c-m-left-content');
+
+
+    //
+    $('.c-m-r-par2-content-text').each(function(i) {
+        $(this).find(".c-m-r-par2-content-text-voteTitle").css({width:$(this).width()-$(this).find(".c-m-r-par2-content-text-voteNumber").outerWidth(true)-5});
+    });
+
+    // 显示/关闭toggle
+    $('.toggle').click(function() {
+        if(!$(this).hasClass('hide')) {
+            $(this).next().stop().slideUp('fast');
+            $(this).addClass('hide');
+        } else {
+            $(this).next().stop().css({'opacity':'1'}).slideDown('fast');
+            $(this).removeClass('hide');
+        }
+    });
+});
